30d367bf1f376b1778eb9b78f9c55b230a1eb5df
Peter Karman <peter@truss.works>
2021-08-23 10:55:36 -0500

    lint, distcheck

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
-setup(
-    name='logindotgov-oidc',
-    packages=find_packages(include=['logindotgov.oidc']),
-    version='0.1.0',
-    description='OpenID Connect Relying Party client',
-    author='Peter Karman peter@truss.works',
-    license='MIT',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    test_suite='tests',
-)
 
+setup(
+    url="https://github.com/trussworks/logindotgov-oidc-py",
+    python_requires=">3.5",
+    name="logindotgov-oidc",
+    packages=find_packages(include=["logindotgov.oidc"]),
+    version="0.1.0",
+    description="OpenID Connect Relying Party client",
+    author="Peter Karman",
+    author_email="peter@truss.works",
+    license="MIT",
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    test_suite="tests",
+)
